539ae2c1504825bdc8f7a8e4cc4be71b4e8ad04f
Caroline <carolinechatelon@hotmail.fr>
2020-12-08 16:32:36 +0100

    add form search id

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,6 +1,12 @@
 <template>
     <div>
         <h1>{{ title }}</h1>
+        <div>
+            <form @submit.prevent="getPokemon()">
+                <input v-model="pokemonId" placeholder="chose ID ">
+                <input type="submit" value="Submit">
+            </form>
+        </div>
     </div>
 </template>
 
