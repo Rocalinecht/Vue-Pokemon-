4ec9df92a8a10b3603ed1544b143e8b44875f661
Caroline <carolinechatelon@hotmail.fr>
2020-12-08 16:56:17 +0100

    add img

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -9,6 +9,9 @@
         </div>
         <div>
             <h2>{{ pokemon.name}}</h2>
+              <img
+                :src= "pokemon.sprites.front_default" 
+                :alt="pokemon.name"  /> 
         </div>
     </div>
 </template>
